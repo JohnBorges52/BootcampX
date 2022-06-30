e53f686fd56db50ffb8cff2e0de84dcc574c0f8b
John <jrborges52@email.com>
2022-06-30 03:41:13 +0000

    add safe way to receive data

---
+++ bootcamp_app/students.js
@@ -0,0 +1,38 @@
+const {Pool} = require('pg');
+
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+
+
+const queryString = (`
+SELECT students.id as id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '$1'
+LIMIT $2;
+`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`,limit]
+
+pool.query(queryString, values)
+.then(response => {
+  response.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+  })
+.catch(err => console.error('query error', err.stack));
+
+
+
+
+
+
